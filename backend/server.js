3cb49290db831b5168657447846e9db349d678e8
Edoardo <edoardo.pirani@gmail.com>
2024-03-08 17:46:03 +0000

    3

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 //import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5001;
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB(); // Connect to MongoDB
 
@@ -14,15 +15,18 @@ app.get('/', (req, res) => {
   res.send('API is Running...');
 });
 
-app.get('api/products', (req,res) => {
-  res.json(products);    
-});
+// app.get('api/products', (req,res) => {
+//   res.json(products);    
+// });
   
-app.get('api/products/:id', (req,res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);    
-});
+// app.get('api/products/:id', (req,res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);    
+// });
     
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log (`server running on port ${port}`)); 
