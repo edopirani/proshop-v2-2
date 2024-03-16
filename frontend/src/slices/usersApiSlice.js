5f9c0a9d4eb9f443258c918dfc206979eb4922d6
Edoardo <edoardo.pirani@gmail.com>
2024-03-16 12:46:04 +0000

    postfeaut

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -9,7 +9,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
         method: 'POST',
         body: data,
       }),
-    }),
+     }),
     register: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}`,
@@ -23,42 +23,44 @@ export const userApiSlice = apiSlice.injectEndpoints({
         method: 'POST',
       }),
     }),
-    profile: builder.mutation({
-      query: (data) => ({
-        url: `${USERS_URL}/profile`,
-        method: 'PUT',
-        body: data,
-      }),
-    }),
-    getUsers: builder.query({
-      query: () => ({
-        url: USERS_URL,
-      }),
-      providesTags: ['User'],
-      keepUnusedDataFor: 5,
-    }),
-    deleteUser: builder.mutation({
-      query: (userId) => ({
-        url: `${USERS_URL}/${userId}`,
-        method: 'DELETE',
-      }),
-    }),
-    getUserDetails: builder.query({
-      query: (id) => ({
-        url: `${USERS_URL}/${id}`,
-      }),
-      keepUnusedDataFor: 5,
-    }),
-    updateUser: builder.mutation({
-      query: (data) => ({
-        url: `${USERS_URL}/${data.userId}`,
-        method: 'PUT',
-        body: data,
-      }),
-      invalidatesTags: ['User'],
-    }),
-  }),
-});
+  })})
+//     }),
+//     profile: builder.mutation({
+//       query: (data) => ({
+//         url: `${USERS_URL}/profile`,
+//         method: 'PUT',
+//         body: data,
+//       }),
+//     }),
+//     getUsers: builder.query({
+//       query: () => ({
+//         url: USERS_URL,
+//       }),
+//       providesTags: ['User'],
+//       keepUnusedDataFor: 5,
+//     }),
+//     deleteUser: builder.mutation({
+//       query: (userId) => ({
+//         url: `${USERS_URL}/${userId}`,
+//         method: 'DELETE',
+//       }),
+//     }),
+//     getUserDetails: builder.query({
+//       query: (id) => ({
+//         url: `${USERS_URL}/${id}`,
+//       }),
+//       keepUnusedDataFor: 5,
+//     }),
+//     updateUser: builder.mutation({
+//       query: (data) => ({
+//         url: `${USERS_URL}/${data.userId}`,
+//         method: 'PUT',
+//         body: data,
+//       }),
+//       invalidatesTags: ['User'],
+//     }),
+//   }),
+//});
 
 export const {
   useLoginMutation,
